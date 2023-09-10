1434b337e8826822e57d6e74a998856d9e6976ab
Ismita Singh <98451067+IsmitaSingh@users.noreply.github.com>
2023-09-10 15:12:13 +0530

    Update 1517. Find Users With Valid E-Mails.sql

--- Advanced String Functions / Regex / Clause/1517. Find Users With Valid E-Mails.sql
+++ Advanced String Functions / Regex / Clause/1517. Find Users With Valid E-Mails.sql
@@ -1,3 +1,2 @@
-SELECT user_id, name, mail 
-FROM Users 
-WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'
+SELECT * from Users 
+WHERE mail REGEXP ('^[a-zA-Z][A-Za-z0-9\-\_\.]*@leetcode[.]com$');
